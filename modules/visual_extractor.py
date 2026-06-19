fd53a7928820c6b8c194b3c80516988517af3515
mahardika.pratama.e <mahardikapratama078@gmail.com>
2026-06-19 14:52:04 +0700

    feat: implement visual_extractor with CoSign hierarchical GCN and masking mechanism

--- modules/visual_extractor.py
+++ modules/visual_extractor.py
@@ -34,8 +34,8 @@ def generate_mask(shape, part_num, clip_length, ratio, dim):
             mask_q[position[0][i], position[1][i], position[2][i]] = 1
         else:
             mask_k[position[0][i], position[1][i], position[2][i]] = 1
-    mask_q = mask_q.astype(np.bool8)
-    mask_k = mask_k.astype(np.bool8)
+    mask_q = mask_q.astype(bool)
+    mask_k = mask_k.astype(bool)
 
     mask_cat_q = torch.ones(shape)
     mask_cat_k = torch.ones(shape)
